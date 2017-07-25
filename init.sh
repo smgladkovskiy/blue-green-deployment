10e2d72fc5db9a691ffc393c3e9a901a82e6f232
Sergey Gladkovskiy <smgladkovskiy@gmail.com>
2017-07-25 15:40:54 +0300

    Использование полного пути при прописывании линка на шабоны рестарта и остановки.

--- init.sh
+++ init.sh
@@ -6,6 +6,7 @@
 success "Start environment initialization..."
 
 # ENV files init
+switchPath=`pwd -P`
 envFile='.env'
 usedPortsFile='.used-ports'
 fileFromExample ${envFile}
@@ -100,8 +101,8 @@ do
         cp ${projectInstancePath}/.env.example ${projectInstancePath}/.env
         sed -i "s,NGINX_HTTP_PORT=80,NGINX_HTTP_PORT=${nextPort},g" ${projectInstancePath}/.env
         sed -i "s,INSTANCE=develop,INSTANCE=${instance},g" ${projectInstancePath}/.env
-        ln -s ./templates/stop.sh ${projectInstancePath}/stop.sh
-        ln -s ./templates/restart.sh ${projectInstancePath}/restart.sh
+        ln -s ${switchPath}/templates/stop.sh ${projectInstancePath}/stop.sh
+        ln -s ${switchPath}/templates/restart.sh ${projectInstancePath}/restart.sh
     done
 done
 
